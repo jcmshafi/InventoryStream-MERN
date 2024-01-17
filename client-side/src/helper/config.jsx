b8af16c5791d6562cf2deb1512d44cd35a895c3f
shafi ujjaman <jcmshafi>
2024-01-18 00:17:00 +0600

    final changes

--- client-side/src/helper/config.jsx
+++ client-side/src/helper/config.jsx
@@ -1,2 +1,2 @@
 // export const BaseURL="http://localhost:5000/api/v1";
-export const BaseURL="https://inventorystream-backend.onrender.com";
+export const BaseURL = "https://inventorystream-backend.onrender.com/api/v1";
